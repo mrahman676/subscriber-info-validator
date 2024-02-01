a547844063b3fb6c531e874ec1db1ed7ddbc8767
Mohammed Rahman <mohammed.rahman676@gmail.com>
2024-02-01 06:41:37 +0000

    got the LUHN algorithm working when user inputs card number

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,7 @@ function validateCard () {
     const cardNumArrayString = cardNumberString.split('');
     const cardNumArray = Array.from(cardNumArrayString, Number);
 
-    console.log(cardNumArray);
+    console.log("Card Number from Form: ", cardNumArray);
     
     if (cardNumArray.length === 16) {
 
@@ -37,36 +37,112 @@ function validateCard () {
 
             if (isNaN(cardNumArray[i])) {
                 alert("Card Number must contain only numerical characters, please re-enter");
-            }
+                return;
+            };
         };
 
-        const newArray = cardNumArray;
-        const realCardNumber = newArray.join('');
-        return realCardNumber;
-
-        
+        console.log("No NaN within Card Number");
 
         // Check the sum of the number is not 0
 
-        // for (j=0; j < cardNumArray.length; i++) {
-        //     if (cardNumArray[i]++ === 0) {
-        //         alert("Card Number should not total to 0, please re-enter");
-        //     }
+        let total = 0;
 
-        //     else {
-        //         return cardNumArray;
-        //     }
-        // }
+        for (j=0; j < cardNumArray.length; j++) {
+            
+            total = total + cardNumArray[j];
+        };
 
+        console.log("Total of Card Numbers: ", total)
+
+        if (total === 0) {
+            alert("Card Number should not total to 0, please re-enter");
+            console.log("The total should not be: ", total);
+            return; };
+
+        // New array for only single digits
         
+        const singleArray = [];
+
+        for (k=cardNumArray.length - 1; k > 0; k -= 2) {
+
+            singleArray.push(cardNumArray[k]);
+        }
+
+        console.log("New Array with only single digits: ", singleArray);
+
+        // Starting from the rightmost digit, double the value of every second digit
+
+        let doubleArray = [];
+        let double;
+
+        for (l=cardNumArray.length -2; l >= 0; l -= 2) {
+
+            double = cardNumArray[l]*2;
+            doubleArray.push(double);
+        }
+
+        console.log("New Array with only double digits: ", doubleArray);
+
+        // const splitArray = [];
+
+        //  If doubling the number results in a two-digit number greater than 9, then add the digits of the product.
+        for (m=0; m < doubleArray.length; m++) {
+            if (doubleArray[m] > 9) {
+                const split = doubleArray[m].toString().split('').map(Number);
+
+                console.log("Array of Split Numbers: ", split)
+
+                let totalSplit = split[0] + split[1];
+                console.log("Split Number Added: ", totalSplit);
+                doubleArray[m] = totalSplit;
+            }
+        }
+
+        console.log("New Array, splitting and combining product of double digits: ", doubleArray);
+
+        const newSingleArray = [];
+
+        for (n=0; n<singleArray.length; n++) {
+            newSingleArray.push(singleArray[n]);
+        }
+
+        console.log(newSingleArray);
+
+        for (o=0; o<doubleArray.length; o++) {
+            newSingleArray.push(doubleArray[o]);
+        }
+
+        console.log("New Array of Single Digits: ", newSingleArray);
+
+        let singleTotal = 0;
+
+        for (p=0; p<newSingleArray.length; p++) {
+            singleTotal = newSingleArray[p] + singleTotal;
+        }
+
+        console.log("Total of All Single Digits: ", singleTotal);
+
+        if (singleTotal % 10 === 0) {
+
+            console.log("Card Number is Valid")
+
+        }
+
+            else {
+            alert("Card Number is not Valid")
+            return;
+            }
         
 
+        const realCardNumber = cardNumArray.join('');
+        console.log(realCardNumber);
+        return (realCardNumber);
     }
         else {
             alert("Your card number must be 16 digits long")
         }; 
+
 }
-    
 
 function submitButton() {
 
